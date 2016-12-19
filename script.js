b00fb725d147fcd1be122d523fbb991266d4e594
Bill Leonhardt <iamleonhardt@gmail.com>
2016-12-18 17:57:19 -0800

    drag drop groups work

---
+++ script.js
@@ -0,0 +1,60 @@
+
+(function() {
+
+    angular
+        .module('app', ['gm.dragDrop'])
+        .run(run);
+
+    function run($rootScope, $filter) {
+
+        $rootScope.group1 = {
+            items: [
+                { name: "Jason" },
+                { name: "Miles" },
+                { name: "Sloan" },
+                { name: "Brian" },
+                { name: "Tammy" },
+                { name: "Sean D" },
+                { name: "Sean G" },
+                { name: "Patrick" },
+                { name: "Mike" },
+                { name: "Vernon" }
+            ]
+        };
+
+        $rootScope.group2 = {
+            items: [
+            ]
+        };
+
+        $rootScope.group3 = {
+            items: [
+            ]
+        };
+
+        $rootScope.group4 = {
+            items: [
+            ]
+        };
+
+
+        $rootScope.onHover = function(item) {
+            return function(dragItem, mouseEvent) {
+                if(item != dragItem)
+                    dragItem.order = item.order + ((mouseEvent.offsetY || -1) > 0 ? 0.5 : -0.5)
+            }
+        };
+
+
+        $rootScope.getDropHandler = function(category) {
+            return function(dragOb) {
+                if(category.items.indexOf(dragOb.item) < 0) {
+                    dragOb.category.items.splice(dragOb.category.items.indexOf(dragOb.item), 1);
+                    category.items.push(dragOb.item);
+                    return true;  // Returning truthy value since we're modifying the view model
+                }
+            }
+        }
+    }
+
+})();
